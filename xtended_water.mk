b8a718e93e4478288c4caa9320ddb8f544baa84d
ninja-ninja-arch <rajustha8027414@gmail.com>
2025-01-18 07:07:46 -0800

    Update and rename lineage_water.mk to xtended_water.mk

--- xtended_water.mk
+++ xtended_water.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2024 The LineageOS Project
+# Copyright (C) 2024 The xtendedOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -7,14 +7,14 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common xtended stuff.
+$(call inherit-product, vendor/xtended/config/common_full_phone.mk)
 
 # Inherit from water device
 $(call inherit-product, device/xiaomi/water/device.mk)
 
 PRODUCT_DEVICE := water
-PRODUCT_NAME := lineage_water
+PRODUCT_NAME := xtended_water
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := water
 PRODUCT_MANUFACTURER := xiaomi

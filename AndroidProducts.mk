af6fd4ec5ace3a22e34b2fe13a1503e4f7c830da
ninja-ninja-arch <rajustha8027414@gmail.com>
2025-01-18 07:08:42 -0800

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,13 @@
 #
-# Copyright (C) 2024 The LineageOS Project
+# Copyright (C) 2024 The xtendedOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_water.mk
+    $(LOCAL_DIR)/xtended_water.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_water-user \
-    lineage_water-userdebug \
-    lineage_water-eng
+    xtended_water-user \
+    xtended_water-userdebug \
+    xtended_water-eng
